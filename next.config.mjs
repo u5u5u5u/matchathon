45072c62859087b6c8de9e9352d221ec0a845e28
u5u5u5u <kyutech.joko@gmail.com>
2024-04-26 22:21:15 +0900

    [add]Sass_install#8

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
 };
 
 export default nextConfig;
