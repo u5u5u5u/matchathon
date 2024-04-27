955d8b8cd06a1debfa2a8ee1e969769c87b9a915
u5u5u5u <kyutech.joko@gmail.com>
2024-04-27 19:27:58 +0900

    [add]ドキュメントの更新テスト#4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { db } from "../../lib/firebase/firebase";
-import { addDoc, collection } from "firebase/firestore";
+import { updateDoc, doc, addDoc, collection } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -27,6 +27,17 @@ const addDataToFirestore = async (
   }
 };
 
+const updateDataInFirestore = async (id: string, newData: any) => {
+  try {
+    await updateDoc(doc(db, "messages", id), newData);
+    console.log("Document updated successfully");
+    return true;
+  } catch (error) {
+    console.error("Error updating document: ", error);
+    return false;
+  }
+};
+
 export default function Home() {
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
@@ -34,11 +45,24 @@ export default function Home() {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!name || !email || !message) return;
     const added = await addDataToFirestore(name, email, message);
     if (added) {
       setName("");
       setEmail("");
       setMessage("");
+    }
+  };
+
+  const updateData = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const updated = await updateDataInFirestore("ADBIuHiTImu7GLZPgPnx", {
+      name: name,
+      email: email,
+      message: message,
+    });
+    if (updated) {
+      console.log("Data updated successfully");
     }
   };
 
@@ -50,7 +74,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body>
+      <main>
         <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="name">Name</label>
@@ -83,7 +107,40 @@ export default function Home() {
             <button type="submit">Submit</button>
           </div>
         </form>
-      </body>
+
+        <form onSubmit={updateData}>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="mail">Email</label>
+            <input
+              type="text"
+              id="mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="message">Message</label>
+            <textarea
+              rows={5}
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+          </div>
+          <div>
+            <button type="submit">Update</button>
+          </div>
+        </form>
+      </main>
     </>
   );
 }
