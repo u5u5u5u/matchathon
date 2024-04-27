9007b4409a92dbaec7937dd41a34b094669f4701
u5u5u5u <kyutech.joko@gmail.com>
2024-04-27 09:07:26 +0900

    [add]firestorの導入とテスト#4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,46 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { db } from "../../lib/firebase/firebase";
+import { addDoc, collection } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const addDataToFirestore = async (
+  name: string,
+  email: string,
+  message: string
+) => {
+  try {
+    const docRef = await addDoc(collection(db, "messages"), {
+      name: name,
+      email: email,
+      message: message,
+    });
+    console.log("Document written with ID: ", docRef.id);
+    return true;
+  } catch (error) {
+    console.error("Error adding document: ", error);
+    return false;
+  }
+};
+
 export default function Home() {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const added = await addDataToFirestore(name, email, message);
+    if (added) {
+      setName("");
+      setEmail("");
+      setMessage("");
+    }
+  };
+
   return (
     <>
       <Head>
@@ -14,6 +50,40 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <body>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="mail">Email</label>
+            <input
+              type="text"
+              id="mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="message">Message</label>
+            <textarea
+              rows={5}
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </body>
     </>
   );
 }
