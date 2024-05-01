b644f6e9d71e26cf8fd1c10f7e4760283e509cbb
u5u5u5u <kyutech.joko@gmail.com>
2024-05-01 11:13:11 +0900

    [change]ログインのコードを変更#2

--- src/pages/firestoreTest/index.tsx
+++ src/pages/firestoreTest/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { db } from "../../../lib/firebase/firebase";
+import { db } from "../../lib/firebase";
 import {
   updateDoc,
   addDoc,
