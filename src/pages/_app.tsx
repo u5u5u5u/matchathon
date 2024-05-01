b3004b200fc837dbc6d5014f90b251d9b962b139
u5u5u5u <kyutech.joko@gmail.com>
2024-05-01 23:44:12 +0900

    [update]トップページのスタイル調整#21

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.module.scss";
 import type { AppProps } from "next/app";
 import { AuthProvider } from "@/context/auth";
+import "../styles/style.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
