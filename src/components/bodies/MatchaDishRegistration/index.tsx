fa070e74f1b7c7fda2e086fb24249e03d7a3a9fe
u5u5u5u <kyutech.joko@gmail.com>
2024-05-15 08:25:47 +0900

    [add]登録項目追加 要検討#5

--- src/components/bodies/MatchaDishRegistration/index.tsx
+++ src/components/bodies/MatchaDishRegistration/index.tsx
@@ -3,7 +3,43 @@ import React from "react";
 const MatchaDishRegistration: React.FC = () => {
   return (
     <>
-      <h1>登録</h1>
+      <form>
+        <div>
+          <label htmlFor="">料理名</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">店舗名</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">住所</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">ジャンル</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">日付</label>
+          <input type="date" />
+        </div>
+        <div>
+          <label htmlFor="">苦味</label>
+          <input type="range" />
+        </div>
+        <div>
+          <label htmlFor="">甘味</label>
+          <input type="range" />
+        </div>
+        <div>
+          <label htmlFor="">濃さ</label>
+          <input type="range" />
+        </div>
+        <div>
+          <button type="submit">登録</button>
+        </div>
+      </form>
     </>
   );
 };
